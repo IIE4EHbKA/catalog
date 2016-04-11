dd90957ab975d8084771886b52ece49c48506184
IIE4EHbKA <sergofan1@yandex.ru>
2016-04-11 18:33:21 +0300

    New view and readme

--- database/migrations/2016_04_11_152112_ProductsTable.php
+++ database/migrations/2016_04_11_152112_ProductsTable.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class ProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function(Blueprint $table)
+        {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name',255);
+            $table->string('category',255);
+            $table->string('price',255);
+            $table->text('desc');
+            $table->text('image');
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('products');
     }
 }
